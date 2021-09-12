fc704aba03457b3581a8fd21cafd138bbb4c6bdd
Henry McArdle <hrmcardle0@yahoo.com>
2021-09-12 14:16:33 -0400

    Update run-latest.py

--- run-latest.py
+++ run-latest.py
@@ -7,9 +7,73 @@ from dateutil.tz import tzlocal
 
 client = boto3.client('ec2')
 
-#are we imported?
 if __name__ != '__main__':
         exit(1)
+        
+#hold scanner information
+class Scanner:
+
+        def getLatestAmi(func):
+                def inner(self):
+                        self._ami = getLatestAmi()
+                        print("Latest AMI is: {}".format(self._ami))
+                        func(self)
+                return inner
+
+
+        def __init__(self, **kwargs):
+                self._tenant = kwargs['tenant']
+                self._vpc = kwargs['vpc']
+                self._subnet = kwargs['subnet']
+                self._sg = kwargs['sg']
+                self._profile = kwargs['profile']
+                self._key = kwargs['key']
+
+        @property
+        def tenant(self):
+                return self._tenant
+
+        @tenant.setter
+        def tenant(self, new_tenant):
+                self._tenant = new_tenant
+
+        @property
+        def vpc(self):
+                return self._vpc
+
+        @property
+        def subnet(self):
+                return self._subnet
+
+        @property
+        def sg(self):
+                return self._sg;
+
+        @property
+        def profile(self):
+                return self._profile
+
+        @property
+        def key(self):
+                return self._key
+
+        @getLatestAmi
+        def runInstance(self):
+                print("Starting Nessus Scanner AMI {} in {} for VPC {}...".format(self._ami, self._tenant, self._vpc))
+
+                #assume role
+                session = assumed_role_session('arn:aws-us-east-1:iam::${AccountID}:role/${RoleName}')
+                ec2 = boto3.client('ec2')
+
+                #launch instance and return id
+                try:
+                        response = client.run_instances(ImageId=self._ami, SubnetId=self._subnet, SecurityGroupIds=[self._sg], InstanceType='t3.medium', MinCount=1, MaxCount=1, TagSpecifications=[{'ResourceType':'instance', 'Tags': [{'Key':'Name', 'Value':'Nessus-Scanner'}]}])
+                        instance_id = response["Instances"][0]["InstanceId"]
+                        print("Successfully Launched Nessus Scanner. ID: {}".format(instance_id))
+                except Exception as e:
+                        print("Error starting instance: {}".format(e))
+                        exit(1)
+       
 
 #tenant-role mapping
 tr_dict = {
@@ -95,71 +159,6 @@ def getLatestAmi():
                 print(e)
         ami = (sorted(response["Images"], key=lambda image: image["CreationDate"]))[-1]["ImageId"]
         return ami
-
-
-#holds scanner information
-class Scanner:
-
-        def getLatestAmi(func):
-                def inner(self):
-                        self._ami = getLatestAmi()
-                        print("Latest AMI is: {}".format(self._ami))
-                        func(self)
-                return inner
-
-
-        def __init__(self, **kwargs):
-                self._tenant = kwargs['tenant']
-                self._vpc = kwargs['vpc']
-                self._subnet = kwargs['subnet']
-                self._sg = kwargs['sg']
-                self._profile = kwargs['profile']
-                self._key = kwargs['key']
-
-        @property
-        def tenant(self):
-                return self._tenant
-
-        @tenant.setter
-        def tenant(self, new_tenant):
-                self._tenant = new_tenant
-
-        @property
-        def vpc(self):
-                return self._vpc
-
-        @property
-        def subnet(self):
-                return self._subnet
-
-        @property
-        def sg(self):
-                return self._sg;
-
-        @property
-        def profile(self):
-                return self._profile
-
-        @property
-        def key(self):
-                return self._key
-
-        @getLatestAmi
-        def runInstance(self):
-                print("Starting Nessus Scanner AMI {} in {} for VPC {}...".format(self._ami, self._tenant, self._vpc))
-
-                #assume role
-                session = assumed_role_session('arn:aws-us-east-1:iam::${AccountID}:role/${RoleName}')
-                ec2 = boto3.client('ec2')
-
-                #launch instance and return id
-                try:
-                        response = client.run_instances(ImageId=self._ami, SubnetId=self._subnet, SecurityGroupIds=[self._sg], InstanceType='t3.medium', MinCount=1, MaxCount=1, TagSpecifications=[{'ResourceType':'instance', 'Tags': [{'Key':'Name', 'Value':'Nessus-Scanner'}]}])
-                        instance_id = response["Instances"][0]["InstanceId"]
-                        print("Successfully Launched Nessus Scanner. ID: {}".format(instance_id))
-                except Exception as e:
-                        print("Error starting instance: {}".format(e))
-                        exit(1)
 
 #parse arguments
 init, tenant, vpc, subnet, sg, profile, key = [r for r in sys.argv]
